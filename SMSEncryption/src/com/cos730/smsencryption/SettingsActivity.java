bf4f1dfd9810bf86de3e456ee8f77566f7586f11
hhvvkk <hvkoesveld@gmail.com>
2014-05-06 22:43:11 +0100

    changed activity name

--- SMSEncryption/src/com/cos730/smsencryption/SettingsActivity.java
+++ SMSEncryption/src/com/cos730/smsencryption/SettingsActivity.java
@@ -25,7 +25,7 @@ public class SettingsActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_update_user);
+		setContentView(R.layout.activity_settings);
 
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
